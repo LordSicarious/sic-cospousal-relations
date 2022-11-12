fc7f5038ad6e27414f9fbbb002f0b97a2f7c1865
LordSicarious <87661780+LordSicarious@users.noreply.github.com>
2022-11-13 01:51:48 +1100

    Fix Romance Bug
    Addresses a bug that caused all romance chance to be fixed at the minimum non-zero value.

--- Source/HarmonyPatches/Postfix_SecondaryLovinChanceFactor.cs
+++ Source/HarmonyPatches/Postfix_SecondaryLovinChanceFactor.cs
@@ -6,10 +6,11 @@ using Verse;
 namespace CoSpousalRelations {
 public static class Postfix_SecondaryLovinChanceFactor {
     // Modifies the "SecondaryLovinChanceFactor" method to permit (rare) spontaneous bisexuality, as existed in earlier versions of the game
-    public static void Patch(Pawn otherPawn, ref float __result, ref Pawn ___pawn) {
+    public static void Patch(Pawn otherPawn, ref float __result, ref Pawn ___pawn, Pawn_RelationsTracker __instance) {
+        if (__result > 0.15f) { return; }
         if (___pawn.def == otherPawn.def && ___pawn != otherPawn) {
             if (___pawn.ageTracker.AgeBiologicalYearsFloat > 16f && otherPawn.ageTracker.AgeBiologicalYearsFloat > 16f) {
-                __result = 0.15f;
+                __result = __instance.LovinAgeFactor(otherPawn) * __instance.PrettinessFactor(otherPawn) * 0.15f;
             }
         }
     }
